0e5b2e35859226efa19574295c8ae4ceb3e9a9f5
unknown <chogalesanika8@gmail.com>
2024-12-24 08:10:08 +0530

    initial

--- CurrencyConverter.js
+++ CurrencyConverter.js
@@ -1,6 +1,10 @@
 document.getElementById("calculate").addEventListener("click",()=>{
     
-    document.getElementById("fromCurrency").value;
-    
+    let fromCurrency=document.getElementById("fromCurrency").value;
+    let toCurrency=document.getElementById("toCurrency").value;
+    let Amount=document.getElementById("amount").value;
 
+    console.log(fromCurrency);
+    console.log(toCurrency);
+    console.log(Amount);
 })
