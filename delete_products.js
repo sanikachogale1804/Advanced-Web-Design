1f3375f4f742954f7288a091911f2fd62bf368b3
unknown <chogalesanika8@gmail.com>
2024-12-30 07:39:57 +0530

    initial

--- delete_products.js
+++ delete_products.js
@@ -1,9 +1,12 @@
 fetch("http://localhost:8080/product")
 .then(data=>data.json())
 .then(data=>{
-    console.log(data)
+    console.log(data);
     data["_embedded"]["products"].forEach((product)=>{
-        console.log(product)
+        console.log(product);
+        const a=document.createElement("a");
+        a.textContent=product("prodcutName");
+        document.body.appendChild(a);
 
     }) 
 })
