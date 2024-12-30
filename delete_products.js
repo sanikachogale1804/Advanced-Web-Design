edd793473dcfa809431935428116418059e2aad9
unknown <chogalesanika8@gmail.com>
2024-12-30 07:51:45 +0530

    initial

--- delete_products.js
+++ delete_products.js
@@ -3,9 +3,13 @@ fetch("http://localhost:8080/product")
 .then(data=>{
     console.log(data);
     data["_embedded"]["products"].forEach((product)=>{
-        console.log(product);
+        // console.log(product);
         const a=document.createElement("a");
         a.textContent=product["productName"];
         document.body.appendChild(a);
+        //setAttribute is js method which will help you
+        console.log(product["_links"]["self"]["href"]);
+        a.setAttribute("href",product["_links"]["self"]["href"]);
+
     }) 
 })
