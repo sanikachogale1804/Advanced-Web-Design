ae361de842c42d95fbed29996aeefdf57afce4bc
unknown <chogalesanika8@gmail.com>
2024-12-30 07:22:19 +0530

    initial

--- delete_products.js
+++ delete_products.js
@@ -1,14 +1,5 @@
 fetch("http://localhost:8080/product")
 .then(data=>data.json())
 .then(data=>{
-    console.log(data);
-    document.write(`<p>number: ${data["page"]["size"]}</p>`)
-    data["_embedded"]["products"].forEach(product=>{
-        // $ means value of
-        document.write(`<h1>${product["productName"]}</h1>`)
-        document.write(`<p>${product["_links"]["self"]["href"]}</p>`)
-        document.write(``)
-    
-    });
-
+    console.log(data)
 })
