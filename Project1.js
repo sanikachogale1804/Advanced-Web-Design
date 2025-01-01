154ba7142dd238a478371cc75a69130f9369a392
unknown <chogalesanika8@gmail.com>
2025-01-01 20:09:41 +0530

    initial

---
+++ Project1.js
@@ -0,0 +1,8 @@
+const quotes=[
+    { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+    { text: "In the end, we will remember not the words of our enemies, but the silence of our friends.", author: "Martin Luther King Jr." },
+    { text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", author: "Ralph Waldo Emerson" },
+    { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" },
+    { text: "It does not matter how slowly you go as long as you do not stop.", author: "Confucius" },
+    { text: "You must be the change you wish to see in the world.", author: "Mahatma Gandhi" }
+]
