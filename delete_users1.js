74c3bc62f888b5a18561f28e84ef5ee37614d2bc
unknown <chogalesanika8@gmail.com>
2025-01-03 20:08:42 +0530

    initial

--- delete_users1.js
+++ delete_users1.js
@@ -1,5 +1,8 @@
 fetch("http://localhost:8080/menuItems")
 .then(data=>data.json())
 .then(data=>{
-    console.log(data["_embedded"]["menuItems"])
+    console.log(data["_embedded"]["menuItems"]);
+    data["_embedded"]["menuItems"].forEach((menuItems)=>{
+        console.log(menuItems)
+    })
 })
